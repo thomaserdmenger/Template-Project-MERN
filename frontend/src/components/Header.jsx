3b555d31fc97e2b03e0d5bba9d3e83c3e118f909
thomaserdmenger <thomas.erdmenger@googlemail.com>
2024-06-10 15:38:55 +0200

    Show Register Button only when user is not verified

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,12 +1,17 @@
 import { Link } from "react-router-dom"
+import { UserContext } from "../context/Context"
+import { useContext } from "react"
 
 const Header = () => {
+  const { user } = useContext(UserContext)
+
   return (
     <header className="flex justify-between items-center mb-12 border-b-[1px] pb-6">
       <Link to="/">Company</Link>
       <nav>
         <div className="flex gap-4 items-center">
-          <Link to="/register">Register</Link>
+          {!user.isVerified && <Link to="/register">Register</Link>}
+
           <Link
             to="/login"
             className="bg-blue-600 text-white py-1 px-2 rounded border border-blue-600 hover:bg-white hover:text-blue-600 transition">
